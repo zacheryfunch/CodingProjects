f5e05442781ab9390768c539cdfe89716b37fc89
zacheryfunch <135885945+zacheryfunch@users.noreply.github.com>
2025-06-09 13:54:11 +0200

    Update script.js
    Now time ticks by => with SetInterval(). 
    ! But now i need to stop=> clearInterval() but i need to set an id. 
    ! How to set the time interval with a dropdown menu? Perhaps input time=

--- ChronoDodoTimer/script.js
+++ ChronoDodoTimer/script.js
@@ -1,59 +1,72 @@
 /*Plan: have multiple timers that follow each other in relation to what round they are in. 
-
-Todo list: 
--Code the timer functionality:
---Select the different elements.
---Modify the timer display.
----Figure out how to change the timer text.             =>Reassign the value of the html element with document.querySelector(#id)
----Figure out how to make the timer countdown seconds   =>setInterval(() => {},1000); //Executes a function every 1000 milliseconds (1sec)
-----Assign a variable to the amount of seconds and insert it into the setInterval. 
-// Execute a function every 1 second
-const intervalId = setInterval(() => {
-    console.log('Interval executed every 1 second');
-}, 1000);
-
+++More features:
 --Make the buttons interact with the display.
 -Add a Timer End () : notification inwindow popup
 --add with a sound
 --add vibration.
 -Figure out how to store the Rounds timer options. ?Maybe Array or list?
--
 */
-/*    list of used ids (Dom Respected positions)
+/* Todo list: 
+-Code the timer functionality:
+x--Select the different elements.
+x--Modify the timer display.    document.queryselector(#id)
+x---Figure out how to change the timer text.             =>Reassign the value of the html element with document.querySelector(#id)
+x---Figure out how to make the timer countdown seconds   =>setInterval(() => {},1000); //Executes a function every 1000 milliseconds (1sec)
+x---Assign a variable to the amount of seconds (time) and insert it into the setInterval. 
+x---Fix amount of zeros in timer display                 => .padStart(2, "0") adds "0" until 2 characters are present.
+x---how to turn the integer into a string                => number.String()
+--how to stop,reset,update timer from list?              => clearInterval() stops the timer but how does it work?
+                    const myInterval = setInterval(myTimer, 1000); clearInterval(myInterval);
+//--Add a dropdown menu to select the timer to run.       => <select> element with options                              
+*/
+
+/*    Info: list of used ids (Dom Respected positions)
      timer(all of the timer elements),
         timerDisplay(00:00:00),
         buttons(div of the 3 buttons), 
-            start, stop, reset,
+            set, start, stop, reset,
         display(div of the words and menus), 
             actionDisplay(text Wait 1 min says what to do),
             currentRound(Round 1 Step 1 2do update it),
             programSelect(drop down menu to select which timer to run)*/
 
-//
-let actionDisplay=document.querySelector("#actionDisplay"); //2do optimise speed by getElementById selector
-let start=document.querySelector("#start");
-let stop=document.querySelector("#stop");
-let timerDisplay=document.querySelector("#timerDisplay");
-let secondAmount
-let time= 125; // stored in seconds
+let actionDisplay = document.querySelector("#actionDisplay"); //2do optimise speed by getElementById selector
+let star = document.querySelector("#start");
+let stop = document.querySelector("#stop");
+let timerDisplay = document.querySelector("#timerDisplay");
+let time = 125; // stored in seconds amount of seconds for the timer to run.
   
+set.onclick= function setTimer(){
+    let second = (time%60).toString().padStart(2, "0"); //display of seconds with 2 digits
+    let minute = Math.floor(time / 60).toString().padStart(2, "0"); //minute display with 2 digits
+    actionDisplay.textContent="Round 1, Step 1 Timer started" + " time is :" + time;;
+    timerDisplay.textContent=`${minute} m: ${second} s`;
+};
 
 start.onclick= function startTimer(){
-    let second = (time%60).toString().padStart(2, "0"); 
-    let minute = Math.floor(time / 60);
+    setInterval(() => {
+        time--; //1 second less (1000 milliseconds)
+
+        let second = (time%60).toString().padStart(2, "0"); 
+        let minute = Math.floor(time / 60).toString().padStart(2, "0");
+        timerDisplay.textContent=`${minute} m: ${second} s`;
+    }, 1000);
+};
+/*debugging variables 
     console.log("Start button clicked");
     console.log("time is: " + time);
     console.log("seconds" + second);
     console.log(minute + " min" + second + " sec");
     console.log(`${minute} : ${second}`);
-    actionDisplay.textContent="Round 1, Step 1 Timer started" + "time is :" + time;;
-    timerDisplay.textContent=`${minute} m: ${second} s`;
-};
+*/
 stop.onclick= function stopTimer(){
     console.log("Stop button clicked");
         time++
     actionDisplay.textContent="Round 1, Step 1 Timer started" + "time is :" + time;
     timerDisplay.textContent=`${minute} m: ${second} s`;
+};
+reset.onclick= function resetTimer(){
+    
 };
 /*function runs every sec; it updates the timerDisplay by one 
  !Possible problem displaying 00:1 instead of 00:01  --Todo later ->    .padStart(2, '0')
